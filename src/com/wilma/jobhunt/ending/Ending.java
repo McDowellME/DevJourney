c4d41e73ef2b57665525eff0d84fa9dbd124074a
Melinda McDowell <melinda.e.mcdowell@outlook.com>
2022-02-24 08:50:37 -0800

    add txt files

--- src/com/wilma/jobhunt/ending/Ending.java
+++ src/com/wilma/jobhunt/ending/Ending.java
@@ -2,8 +2,10 @@ package com.wilma.jobhunt.ending;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 //Using this class to store the different endings
 public class Ending {
@@ -11,45 +13,52 @@ public class Ending {
     //this is the ending we'll use when the user explores the glowing rock
     public static void alienMessage() throws InterruptedException {
 
-        String art =
-                        " ██████╗  █████╗ ███╗   ███╗███████╗     ██████╗ ██╗   ██╗███████╗██████╗ \n" +
-                        "██╔════╝ ██╔══██╗████╗ ████║██╔════╝    ██╔═══██╗██║   ██║██╔════╝██╔══██╗\n" +
-                        "██║  ███╗███████║██╔████╔██║█████╗      ██║   ██║██║   ██║█████╗  ██████╔╝\n" +
-                        "██║   ██║██╔══██║██║╚██╔╝██║██╔══╝      ██║   ██║╚██╗ ██╔╝██╔══╝  ██╔══██╗\n" +
-                        "╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗    ╚██████╔╝ ╚████╔╝ ███████╗██║  ██║\n" +
-                        " ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝\n" +
-                        "                                                                          ";
+//        String art =
+//                        " ██████╗  █████╗ ███╗   ███╗███████╗     ██████╗ ██╗   ██╗███████╗██████╗ \n" +
+//                        "██╔════╝ ██╔══██╗████╗ ████║██╔════╝    ██╔═══██╗██║   ██║██╔════╝██╔══██╗\n" +
+//                        "██║  ███╗███████║██╔████╔██║█████╗      ██║   ██║██║   ██║█████╗  ██████╔╝\n" +
+//                        "██║   ██║██╔══██║██║╚██╔╝██║██╔══╝      ██║   ██║╚██╗ ██╔╝██╔══╝  ██╔══██╗\n" +
+//                        "╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗    ╚██████╔╝ ╚████╔╝ ███████╗██║  ██║\n" +
+//                        " ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝\n" +
+//                        "                                                                          ";
+//
+//        System.out.print(art);
 
-        System.out.print(art);
-        String art2 = "\t\n" +
-                ".     .       .  .   . .   .   . .    +  .\n" +
-                "  .     .  :     .    .. :. .___---------___.\n" +
-                "       .  .   .    .  :.:. _\".^ .^ ^.  '.. :\"-_. .\n" +
-                "    .  :       .  .  .:../:            . .^  :.:\\.\n" +
-                "        .   . :: +. :.:/: .   .    .        . . .:\\\n" +
-                " .  :    .     . _ :::/:               .  ^ .  . .:\\\n" +
-                "  .. . .   . - : :.:./:                        .  .:\\\n" +
-                "  .      .     . :..|:                    .  .  ^. .:|\n" +
-                "    .       . : : ..||        .                . . !:|\n" +
-                "  .     . . . ::. ::\\(                           . :)/\n" +
-                " .   .     : . : .:.|. ######              .#######::|\n" +
-                "  :.. .  :-  : .:  ::|.#######           ..########:|\n" +
-                " .  .  .  ..  .  .. :\\ ########          :######## :/\n" +
-                "  .        .+ :: : -.:\\ ########       . ########.:/\n" +
-                "    .  .+   . . . . :.:\\. #######       #######..:/\n" +
-                "      :: . . . . ::.:..:.\\           .   .   ..:/\n" +
-                "   .   .   .  .. :  -::::.\\.       | |     . .:/\n" +
-                "      .  :  .  .  .-:.\":.::.\\             ..:/\n" +
-                " .      -.   . . . .: .:::.:.\\.           .:/\n" +
-                ".   .   .  :      : ....::_:..:\\   ___.  :/\n" +
-                "   .   .  .   .:. .. .  .: :.:.:\\       :/\n" +
-                "     +   .   .   : . ::. :.:. .:.|\\  .:/|\n" +
-                "     .         +   .  .  ...:: ..|  --.:|\n" +
-                ".      . . .   .  .  . ... :..:..\"(  ..)\n";
 
-        System.out.print(art2 + "\n");
 
-        System.out.println("Silly human, you foolishly inspected the glowing rock.\nYou are now a puppet of the Glore Gax alien race....\n\n");//character name getter will be added into with this statement
+
+//        String art2 = "\t\n" +
+//                ".     .       .  .   . .   .   . .    +  .\n" +
+//                "  .     .  :     .    .. :. .___---------___.\n" +
+//                "       .  .   .    .  :.:. _\".^ .^ ^.  '.. :\"-_. .\n" +
+//                "    .  :       .  .  .:../:            . .^  :.:\\.\n" +
+//                "        .   . :: +. :.:/: .   .    .        . . .:\\\n" +
+//                " .  :    .     . _ :::/:               .  ^ .  . .:\\\n" +
+//                "  .. . .   . - : :.:./:                        .  .:\\\n" +
+//                "  .      .     . :..|:                    .  .  ^. .:|\n" +
+//                "    .       . : : ..||        .                . . !:|\n" +
+//                "  .     . . . ::. ::\\(                           . :)/\n" +
+//                " .   .     : . : .:.|. ######              .#######::|\n" +
+//                "  :.. .  :-  : .:  ::|.#######           ..########:|\n" +
+//                " .  .  .  ..  .  .. :\\ ########          :######## :/\n" +
+//                "  .        .+ :: : -.:\\ ########       . ########.:/\n" +
+//                "    .  .+   . . . . :.:\\. #######       #######..:/\n" +
+//                "      :: . . . . ::.:..:.\\           .   .   ..:/\n" +
+//                "   .   .   .  .. :  -::::.\\.       | |     . .:/\n" +
+//                "      .  :  .  .  .-:.\":.::.\\             ..:/\n" +
+//                " .      -.   . . . .: .:::.:.\\.           .:/\n" +
+//                ".   .   .  :      : ....::_:..:\\   ___.  :/\n" +
+//                "   .   .  .   .:. .. .  .: :.:.:\\       :/\n" +
+//                "     +   .   .   : . ::. :.:. .:.|\\  .:/|\n" +
+//                "     .         +   .  .  ...:: ..|  --.:|\n" +
+//                ".      . . .   .  .  . ... :..:..\"(  ..)\n";
+//
+//        System.out.print(art2 + "\n");
+
+        readTextFile("resources/GameOver.txt");
+        readTextFile("resources/Alien.txt");
+
+        System.out.println("\nSilly human, you foolishly inspected the glowing rock.\nYou are now a puppet of the Glore Gax alien race....\n\n");//character name getter will be added into with this statement
 
         try {
             Clip clip = AudioSystem.getClip();
@@ -62,6 +71,15 @@ public class Ending {
             e.printStackTrace();
         }
 
+    }
 
+    private static void readTextFile(String resourceFilePath) {
+        try {
+            String image = Files.readString(Path.of(resourceFilePath));
+            System.out.println(image);
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
